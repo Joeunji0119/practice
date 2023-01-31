9bf30e6d5f493d3f731cc5d5e3b78ace1ab90c1a
Eunji Jo <0119eunji@gmail.com>
2023-01-31 18:50:23 +0900

    모달 css

--- src/Components/Modal/Modal.tsx
+++ src/Components/Modal/Modal.tsx
@@ -5,7 +5,15 @@ import { css } from "@emotion/react";
 const Modal = () => {
   return (
     <ModalContainer>
-      <div css={layout}>aadsfadf</div>
+      <div css={layout}>
+        <div css={modalLayout}>
+          <div css={question}> 창을 닫으시겠습니까 </div>
+          <div css={buttonContainer}>
+            <button css={button}> 네 </button>
+            <button css={button}> 아니오 </button>
+          </div>
+        </div>
+      </div>
     </ModalContainer>
   );
 };
@@ -14,5 +22,36 @@ export default Modal;
 
 const layout = css({
   width: "100vw",
+  minWidth: "600px",
   height: "100vh",
+
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 });
+
+const modalLayout = css({
+  width: "30%",
+  height: "30%",
+  background: "pink",
+
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "space-evenly",
+
+  padding: "2%",
+});
+
+const question = css({
+  fontSize: "20px",
+  fontWeight: 600,
+});
+
+const buttonContainer = css({
+  width: "100%",
+  display: "flex",
+  justifyContent: "space-evenly",
+});
+
+const button = css({});
